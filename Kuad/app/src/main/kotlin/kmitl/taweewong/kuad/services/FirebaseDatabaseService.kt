98876049fb3d6782edac431d4e75ce4ad1d90911
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-02-16 17:27:53 +0700

    created function createBottle in FirebaseDatabaseService

--- Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/services/FirebaseDatabaseService.kt
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/services/FirebaseDatabaseService.kt
@@ -1,6 +1,7 @@
 package kmitl.taweewong.kuad.services
 
 import com.google.firebase.database.FirebaseDatabase
+import kmitl.taweewong.kuad.models.Bottle
 import kmitl.taweewong.kuad.models.User
 
 object FirebaseDatabaseService {
@@ -10,10 +11,21 @@ object FirebaseDatabaseService {
         fun onUpdateUserFailed(message: String)
     }
 
+    interface OnCreateBottleComplete {
+        fun onCreateBottleSuccess(bottle: Bottle)
+        fun onCreateBottleFailed(message: String)
+    }
+
     private const val CHILD_USERS = "users"
+    private const val CHILD_BOTTLES = "bottles"
 
     private val firebaseDatabase = FirebaseDatabase.getInstance()
     private val usersRef = firebaseDatabase.getReference(CHILD_USERS)
+    private val bottlesRef = firebaseDatabase.getReference(CHILD_BOTTLES)
+
+    private fun generateBottleId(): String {
+        return bottlesRef.push().key
+    }
 
     fun updateUser(user: User, listener: OnUpdateUserComplete) {
         usersRef.child(user.id).setValue(user).addOnCompleteListener { task ->
@@ -22,6 +34,21 @@ object FirebaseDatabaseService {
             } else {
                 task.addOnFailureListener { exception ->
                     listener.onUpdateUserFailed(exception.message?: "No error message")
+                }
+            }
+        }
+    }
+
+    fun createBottle(ownerId: String, bottleTitle: String, listener: OnCreateBottleComplete) {
+        val bottleId = generateBottleId()
+        val newBottle = Bottle(bottleId, bottleTitle, ownerId)
+
+        bottlesRef.child(bottleId).setValue(newBottle).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                listener.onCreateBottleSuccess(newBottle)
+            } else {
+                task.addOnFailureListener { exception ->
+                    listener.onCreateBottleFailed(exception.message?: "No error message")
                 }
             }
         }
