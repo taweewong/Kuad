4c9d4c71420e28b9e3ed73a1f7695ba649d74897
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-02-19 00:31:09 +0700

    added firebase function randomBottle

--- FirebaseFunction/functions/index.js
+++ FirebaseFunction/functions/index.js
@@ -6,3 +6,14 @@ const ref = admin.database().ref()
 exports.helloKuad = functions.https.onRequest((request, response) => {
     response.send("Hello from Kuad!");
 });
+
+exports.randomBottle = functions.https.onRequest((request, response) => {
+    ref.child('bottles').orderByChild('hasOwner').equalTo(false).once('value')
+    .then(snap => {
+        response.send(snap)
+        return snap
+    }).catch(error => {
+        console.log(error)
+        response.send(error)
+    })
+});
