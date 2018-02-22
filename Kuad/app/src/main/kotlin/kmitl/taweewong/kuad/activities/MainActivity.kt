da234d958844a0cd73cb9c4e8a44421fd6abd03a
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-02-22 14:57:14 +0700

    created function collectData in MainActivity

--- Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/activities/MainActivity.kt
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/activities/MainActivity.kt
@@ -3,12 +3,20 @@ package kmitl.taweewong.kuad.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kmitl.taweewong.kuad.R
+import kmitl.taweewong.kuad.descriptions.KeyName.USER_ID_EXTRA_NAME
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        collectData()
+    }
+
+    private fun collectData() {
+        userId = intent.getStringExtra(USER_ID_EXTRA_NAME)
     }
 }
