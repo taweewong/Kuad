e031068de8a7bd7b18aade5484cf3855fd5b1955
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-02-22 14:56:51 +0700

    created function for check current user and call login service in LoginActivity

--- Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/activities/LoginActivity.kt
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/activities/LoginActivity.kt
@@ -1,22 +1,72 @@
 package kmitl.taweewong.kuad.activities
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import kmitl.taweewong.kuad.R
+import kmitl.taweewong.kuad.descriptions.KeyName.USER_ID_EXTRA_NAME
+import kmitl.taweewong.kuad.services.FirebaseAuthService
+import kmitl.taweewong.kuad.services.FirebaseAuthService.OnSignInComplete
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
 
-        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        checkCurrentUser()
+        initializeView()
+    }
 
-        signUpButton.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+    private fun initializeView() {
+        loginButton.setOnClickListener(this)
+        signUpButton.setOnClickListener(this)
+    }
+
+    private fun checkCurrentUser() {
+        val currentUserId = FirebaseAuthService.getCurrentUserId()
+
+        if (currentUserId != null) {
+            startMainActivity(currentUserId)
         }
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.loginButton -> login()
+            R.id.signUpButton -> startActivity(RegisterActivity())
+        }
+    }
+
+    private fun login() {
+        val email = emailLoginEditText.text.toString()
+        val password = passwordLoginEditText.text.toString()
+
+        FirebaseAuthService.signInWithEmail(email, password, object : OnSignInComplete {
+            override fun onSignInSuccess(uid: String) {
+                startMainActivity(uid)
+            }
+
+            override fun onSignInFailed(message: String) {
+                Toast.makeText(this@LoginActivity, message, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    private fun startActivity(activity: AppCompatActivity) {
+        startActivity(Intent(this@LoginActivity, activity::class.java))
+        finish()
+    }
+
+    private fun startMainActivity(uid: String) {
+        val intent = Intent(this, MainActivity::class.java)
+
+        intent.putExtra(USER_ID_EXTRA_NAME, uid)
+        startActivity(intent)
+        finish()
     }
 }
