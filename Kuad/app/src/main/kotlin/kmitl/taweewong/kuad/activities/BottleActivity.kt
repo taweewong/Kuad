6ba8283e3d0e3bb6d629faa521ab093af7b6bc7f
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-03-01 18:27:48 +0700

    created BottleActivity

---
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/activities/BottleActivity.kt
@@ -0,0 +1,13 @@
+package kmitl.taweewong.kuad.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kmitl.taweewong.kuad.R
+
+class BottleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottle)
+    }
+}
