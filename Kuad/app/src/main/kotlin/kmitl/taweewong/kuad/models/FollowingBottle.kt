19d08a890c69e32c2a76b59406071d3951ca7438
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-03-02 14:22:03 +0700

    edited class FollowingBottle constructor

--- Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/models/FollowingBottle.kt
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/models/FollowingBottle.kt
@@ -1,7 +1,7 @@
 package kmitl.taweewong.kuad.models
 
 data class FollowingBottle(
-        val id: String,
-        val bottleId: String,
-        val title: String
+        val id: String = "",
+        val bottleId: String = "",
+        val title: String = ""
 )
