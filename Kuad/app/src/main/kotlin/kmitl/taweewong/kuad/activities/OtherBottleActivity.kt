ebfe226ff36619124e7074a232776a6a6b7134f7
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-03-06 17:38:58 +0700

    created OtherBottleActivity

---
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/activities/OtherBottleActivity.kt
@@ -0,0 +1,38 @@
+package kmitl.taweewong.kuad.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kmitl.taweewong.kuad.R
+import kmitl.taweewong.kuad.activities.OtherBottleActivity.BottleMenu.FOLLOWING
+import kmitl.taweewong.kuad.activities.OtherBottleActivity.BottleMenuExtraKey.BOTTLE_MENU_EXTRA_KEY
+import kotlinx.android.synthetic.main.activity_other_bottle.*
+
+class OtherBottleActivity : AppCompatActivity() {
+    private lateinit var menu: BottleMenu
+
+    enum class BottleMenu {
+        FOLLOWING, HISTORY
+    }
+
+    object BottleMenuExtraKey {
+        const val BOTTLE_MENU_EXTRA_KEY = "bottleMenu"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_bottle)
+        supportActionBar?.hide()
+
+        collectData()
+
+        if (menu == FOLLOWING) {
+            otherBottleTitle.text = getString(R.string.following_bottles)
+        } else {
+            otherBottleTitle.text = getString(R.string.history_bottles)
+        }
+    }
+
+    private fun collectData() {
+        menu = BottleMenu.valueOf(intent.getStringExtra(BOTTLE_MENU_EXTRA_KEY))
+    }
+}
