b1958b2f10a2f0ead2874b80d8858d2cd65eef65
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-03-06 17:39:18 +0700

    created OtherBottleAdapter and assets

---
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/adapters/OtherBottleAdapter.kt
@@ -0,0 +1,25 @@
+package kmitl.taweewong.kuad.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import kmitl.taweewong.kuad.R
+import kmitl.taweewong.kuad.models.BottleReference
+import kmitl.taweewong.kuad.viewholders.OtherBottleViewHolder
+
+class OtherBottleAdapter(private val bottleRefs: ArrayList<BottleReference>): RecyclerView.Adapter<OtherBottleViewHolder>() {
+    override fun getItemCount(): Int {
+        return bottleRefs.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): OtherBottleViewHolder {
+        val inflater = LayoutInflater.from(parent?.context)
+        val itemView = inflater.inflate(R.layout.other_bottle_layout, parent, false)
+
+        return OtherBottleViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: OtherBottleViewHolder, position: Int) {
+        holder.otherBottleItemTitle.text = bottleRefs[position].title
+    }
+}
---
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/viewholders/OtherBottleViewHolder.kt
@@ -0,0 +1,10 @@
+package kmitl.taweewong.kuad.viewholders
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+import kmitl.taweewong.kuad.R
+
+class OtherBottleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val otherBottleItemTitle: TextView = itemView.findViewById(R.id.otherBottleItemTitle)
+}
