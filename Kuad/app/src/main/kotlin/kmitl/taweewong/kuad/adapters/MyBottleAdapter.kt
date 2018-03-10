3da7ab53d4717c7fe14c30084c449bd234cdf2b3
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-03-10 21:58:23 +0700

    updated android support version from 27.0.2 to 27.1.0

--- Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/adapters/MyBottleAdapter.kt
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/adapters/MyBottleAdapter.kt
@@ -8,12 +8,13 @@ import kmitl.taweewong.kuad.models.Bottle
 import kmitl.taweewong.kuad.viewholders.MyBottleViewHolder
 
 class MyBottleAdapter(private val bottles: ArrayList<Bottle>): RecyclerView.Adapter<MyBottleViewHolder>() {
+
     override fun getItemCount(): Int {
         return bottles.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyBottleViewHolder {
-        val inflater = LayoutInflater.from(parent?.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyBottleViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
         val itemView = inflater.inflate(R.layout.my_bottle_layout, parent, false)
 
         return MyBottleViewHolder(itemView)
--- Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/adapters/OtherBottleAdapter.kt
+++ Kuad/app/src/main/kotlin/kmitl/taweewong/kuad/adapters/OtherBottleAdapter.kt
@@ -8,12 +8,13 @@ import kmitl.taweewong.kuad.models.BottleReference
 import kmitl.taweewong.kuad.viewholders.OtherBottleViewHolder
 
 class OtherBottleAdapter(private val bottleRefs: ArrayList<BottleReference>): RecyclerView.Adapter<OtherBottleViewHolder>() {
+
     override fun getItemCount(): Int {
         return bottleRefs.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): OtherBottleViewHolder {
-        val inflater = LayoutInflater.from(parent?.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OtherBottleViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
         val itemView = inflater.inflate(R.layout.other_bottle_layout, parent, false)
 
         return OtherBottleViewHolder(itemView)
